2953059dda0f4cedebf43742a2f6cb3bebe8c7c1
pbure44 <pbure.ua@gmail.com>
2024-02-16 21:56:20 +0200

    homework 2

--- src/App.js
+++ src/App.js
@@ -1,8 +1,33 @@
+import {useRef, useState} from "react";
+
 const App = () => {
+
+    const name = useRef();
+    const age = useRef();
+    const [user,setUser]=useState({});
+    const save = (e) => {
+        e.preventDefault();
+        // console.log(name.current.value);
+        // console.log(age.current.value);
+        console.log(e.target.name.value);
+        console.log(e.target.age.value);
+    }
+
+    console.log(user);
+
+    const handler =(e)=>{
+        setUser(prevState => ({...prevState,[e.target.name]:e.target.value}))
+    }
+
     return (
         <div>
-            App
+            <form onSubmit={save}>
+                <input type="text" placeholder={'name'} name={'name'} onChange={handler}/>
+                <input type="text" placeholder={'age'} name={'age'} onChange={handler}/>
+                <button>Save</button>
+            </form>
         </div>
+
     );
 };
 
